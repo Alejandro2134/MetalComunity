a971b3261b2fad69af84578a2a9e51c722b24fc7
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-07-24 14:26:39 -0500

    JWT  key

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ require('dotenv').config({
 
 const config = {
     dbUrl: `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0-rtsr4.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    userJwtSecret: process.env.AUTH_USER_JWT_SECRET,
 }
 
 module.exports = config;
