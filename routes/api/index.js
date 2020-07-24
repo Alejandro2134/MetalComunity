8c45e4c5f94bb5bde0373d27170dcbf2e795f406
Alejandro2134 <zapatamolinaalejandro@gmail.com>
2020-07-24 14:27:03 -0500

    Api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.json({
         albums: 'http://localhost:3000/api/albums',
+        comments: 'http://localhost:3000/api/comments',
+        reviews: 'http://localhost:3000/api/reviews'
     })
 })
 
